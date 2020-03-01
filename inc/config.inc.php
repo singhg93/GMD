cd1c5c998c04c028dba6531317ecb4475549ca77
singhg93 <singhg93@student.douglascollege.ca>
2020-03-01 10:57:14 -0800

    changed the config settings

--- inc/config.inc.php
+++ inc/config.inc.php
@@ -5,7 +5,7 @@ define('DB_HOST','ec2-184-72-236-3.compute-1.amazonaws.com,5432');
 
 // define('DB_USER','root');
 define('DB_USER','gxbmqwvkctaspr');
-define('DB_PASS','newTeamGMDPass123');
+define('DB_PASS','ebb486cfada95862501ec1d6cdfe99a6c8301c59b9c26cafa9921159ddd0c75c');
 define('DB_NAME','d3oqtqvhtp312');
 
 ini_set('error_log','log/error.log'); //set error log file destination
