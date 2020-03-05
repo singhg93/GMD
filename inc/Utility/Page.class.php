36d9a19cc0c084c9edaa783265c3d3fb3c858c29
singhg93 <singhg93@student.douglascollege.ca>
2020-03-04 23:56:31 -0800

    changed the footer in Page class

--- inc/Utility/Page.class.php
+++ inc/Utility/Page.class.php
@@ -69,7 +69,6 @@ class Page {
     static function footer(){?>
         
         </div>
-<h6 class="footer text-center bg-primary text-light">Created by : Melodie Lucero, Darren Singh, Gursewak Singh</h6>
             <!-- Optional JavaScript -->
             <!-- jQuery first, then Popper.js, then Bootstrap JS -->
             <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
